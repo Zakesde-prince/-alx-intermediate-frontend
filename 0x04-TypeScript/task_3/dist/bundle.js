ad533003b1e568870c1e9125e489a9cc9bb5a0b0
Zakesde-prince <prince.feela2905@gmail.com>
2025-07-22 02:55:21 +0000

    .

---
+++ 0x04-TypeScript/task_3/dist/bundle.js
@@ -0,0 +1,96 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./js/crud.js":
+/*!********************!*\
+  !*** ./js/crud.js ***!
+  \********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   deleteRow: () => (/* binding */ deleteRow),\n/* harmony export */   insertRow: () => (/* binding */ insertRow),\n/* harmony export */   updateRow: () => (/* binding */ updateRow)\n/* harmony export */ });\n// task_3/js/crud.js\r\n\r\nfunction insertRow(row) {\r\n  console.log('Insert row', row);\r\n  return Math.floor(Math.random() * Math.floor(1000));\r\n}\r\n\r\nfunction deleteRow(rowId) {\r\n  console.log('Delete row id', rowId);\r\n  return;\r\n}\r\n\r\nfunction updateRow(rowId, row) {\r\n  console.log(`Update row ${rowId}`, row);\r\n  return rowId;\r\n}\r\n\n\n//# sourceURL=webpack://task_3/./js/crud.js?\n}");
+
+/***/ }),
+
+/***/ "./js/main.ts":
+/*!********************!*\
+  !*** ./js/main.ts ***!
+  \********************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+eval("{\r\n/// <reference path=\"./crud.d.ts\" />\r\nvar __assign = (this && this.__assign) || function () {\r\n    __assign = Object.assign || function(t) {\r\n        for (var s, i = 1, n = arguments.length; i < n; i++) {\r\n            s = arguments[i];\r\n            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))\r\n                t[p] = s[p];\r\n        }\r\n        return t;\r\n    };\r\n    return __assign.apply(this, arguments);\r\n};\r\nexports.__esModule = true;\r\nvar CRUD = __webpack_require__(/*! ./crud */ \"./js/crud.js\");\r\nvar row = {\r\n    firstName: 'Guillaume',\r\n    lastName: 'Salva'\r\n};\r\nvar newRowID = CRUD.insertRow(row);\r\nvar updatedRow = __assign(__assign({}, row), { age: 23 });\r\nCRUD.updateRow(newRowID, updatedRow);\r\nCRUD.deleteRow(newRowID);\r\n\n\n//# sourceURL=webpack://task_3/./js/main.ts?\n}");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__("./js/main.ts");
+/******/ 	
+/******/ })()
+;
