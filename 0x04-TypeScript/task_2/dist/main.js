616a413dfc50b43fac6f7514bf51b6ec50ebe27a
Zakesde-prince <prince.feela2905@gmail.com>
2025-07-22 01:49:32 +0000

    add

---
+++ 0x04-TypeScript/task_2/dist/main.js
@@ -0,0 +1,43 @@
+"use strict";
+// Classes implementing the interfaces
+var Director = /** @class */ (function () {
+    function Director() {
+    }
+    Director.prototype.workFromHome = function () {
+        return 'Working from home';
+    };
+    Director.prototype.getCoffeeBreak = function () {
+        return 'Getting a coffee break';
+    };
+    Director.prototype.workDirectorTasks = function () {
+        return 'Getting to director tasks';
+    };
+    return Director;
+}());
+var Teacher = /** @class */ (function () {
+    function Teacher() {
+    }
+    Teacher.prototype.workFromHome = function () {
+        return 'Cannot work from home';
+    };
+    Teacher.prototype.getCoffeeBreak = function () {
+        return 'Cannot have a break';
+    };
+    Teacher.prototype.workTeacherTasks = function () {
+        return 'Getting to work';
+    };
+    return Teacher;
+}());
+// Function to create either a Teacher or Director based on salary
+function createEmployee(salary) {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    }
+    else {
+        return new Director();
+    }
+}
+// Testing
+console.log(createEmployee(200)); // Teacher instance
+console.log(createEmployee(1000)); // Director instance
+console.log(createEmployee('$500')); // Director instance
